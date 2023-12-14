94f65ffb817607076c48f52b6db5555b2663f5f4
salman <salmanalijijiga@gmail.com>
2023-12-14 17:44:50 +0400

    javascript file created

---
+++ src/script.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Get references to elements
+//     var toggleBtn = document.getElementById('menuBtn');
+//     var menuOverlay = document.querySelector('.menu_overlay');
+
+//     // Add change event listener to the checkbox
+//     toggleBtn.addEventListener('change', function () {
+//         // Toggle the menu overlay's display property
+//         if (toggleBtn.checked) {
+//             menuOverlay.style.display = 'flex';
+//         } else {
+//             menuOverlay.style.display = 'none';
+//         }
+//     });
+// });
