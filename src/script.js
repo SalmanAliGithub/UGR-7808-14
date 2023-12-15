835c0b97fe839243b2f1978a3893aebbe1874823
salman <salmanalijijiga@gmail.com>
2023-12-15 19:19:16 +0400

    javascript for toggle menu

--- src/script.js
+++ src/script.js
@@ -1,15 +1,19 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Get references to elements
-//     var toggleBtn = document.getElementById('menuBtn');
-//     var menuOverlay = document.querySelector('.menu_overlay');
 
-//     // Add change event listener to the checkbox
-//     toggleBtn.addEventListener('change', function () {
-//         // Toggle the menu overlay's display property
-//         if (toggleBtn.checked) {
-//             menuOverlay.style.display = 'flex';
-//         } else {
-//             menuOverlay.style.display = 'none';
-//         }
-//     });
-// });
+const burger = document.getElementById("menu-burger");
+const landingPage = document.getElementById("myNav");
+const times = document.getElementById("closebtn");
+const brand = document.getElementById("logo");
+
+burger.addEventListener('click', () => {
+    landingPage.style.width = "100%";
+    landingPage.style.display = "block";
+    burger.style.display = "none";
+    brand.style.display = "block";
+})
+
+times.addEventListener('click', () => {
+    landingPage.style.width = "0%";
+    burger.style.display = "flex";
+    landingPage.style.display = "none";
+    brand.style.display = "block";
+})
