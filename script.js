f0d81dc9686cd4c531ff36e16c66af69035a81b5
salman <salmanalijijiga@gmail.com>
2023-12-17 01:16:01 +0400

    javascript linked

---
+++ script.js
@@ -0,0 +1,6 @@
+const burger = querySelector('#burger');
+const landing = querySelector('#landing-page');
+const items = querySelector('.navbar-items');
+
+burger.addEventListener('click', () => {
+})
