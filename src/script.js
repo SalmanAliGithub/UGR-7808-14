d3a3fcd83fa867bf52e011408819076b10485969
salman <salmanalijijiga@gmail.com>
2023-12-18 19:52:20 +0400

    made nav disappear when at the top of screen

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-
+// navigation menu for phone (burger-nav)
 const burger = document.getElementById("menu-burger");
 const landingPage = document.getElementById("myNav");
 const times = document.getElementById("closebtn");
@@ -17,3 +17,21 @@ times.addEventListener('click', () => {
     landingPage.style.display = "none";
     brand.style.display = "block";
 })
+
+
+// on top of the screen the menu tab disappears and displays when start scrolling
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.querySelector(".menu-container");
+    var landing = document.querySelector(".landing-page");
+    document.addEventListener("scroll", function () {
+        var scrollPosition = window.scrollY;
+
+        if (scrollPosition === 0) {
+            header.style.background = "linear-gradient(to right, #67b5ff, #a0e4fa)";
+        } else {
+            header.style.backgroundColor = "#a0e4fa";
+        }
+    });
+});
+
+
