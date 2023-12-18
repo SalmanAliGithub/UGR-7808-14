aa0b67b747395f33169b4e5ba7d39fcad8919d37
salman <salmanalijijiga@gmail.com>
2023-12-18 16:08:53 +0400

    script edited

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const burger = querySelector('#burger');
-const landing = querySelector('#landing-page');
-const items = querySelector('.navbar-items');
+const burger = document.querySelector('#burger');
+const landing = document.querySelector('#landing-page');
+const items = document.querySelector('.navbar-menu');
 
 burger.addEventListener('click', () => {
-})
+    landing.classList.toggle('is-active');
+    items.style.width = "100%";
+});
