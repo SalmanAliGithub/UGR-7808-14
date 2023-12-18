4a8185e170e807ffec7711976774a917f6f67a10
salman <salmanalijijiga@gmail.com>
2023-12-18 16:24:46 +0400

    removed javascript

--- script.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector('#burger');
-const landing = document.querySelector('#landing-page');
-const items = document.querySelector('.navbar-menu');
-
-burger.addEventListener('click', () => {
-    landing.classList.toggle('is-active');
-    items.style.width = "100%";
-});
